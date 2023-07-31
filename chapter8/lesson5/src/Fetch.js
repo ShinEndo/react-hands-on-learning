9aed48ed26fd9f591db2aa28dfdd828ba0d3e37d
ShinEndo <endo@elplanning.co.jp>
2023-07-31 09:02:38 +0900

    データ - 複数のリクエスト

---
+++ chapter8/lesson5/src/Fetch.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useFetch } from './useFetch'
+
+export const Fetch = ({
+    uri,
+    renderSuccess,
+    loadingFallback = <p>loading...</p>,
+    renderError = error => (
+        <pre>{JSON.stringify(error,null,2)}</pre>
+    )
+}) => {
+    const {loading, data, error} = useFetch(uri);
+    if(error) return renderError(error);
+    if(loading) return loadingFallback;
+    if(data) return renderSuccess({data});
+}

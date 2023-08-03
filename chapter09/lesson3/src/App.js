513e15efd11d386c18d62c5cd3cfb7791b721003
ShinEndo <endo@elplanning.co.jp>
2023-08-04 08:59:39 +0900

    Promiseをthrowする

--- chapter09/lesson3/src/App.js
+++ chapter09/lesson3/src/App.js
@@ -4,13 +4,29 @@ import GridLoader from "react-spinners/GridLoader";
 
 // const loadStatus = () => "success - ready";
 const loadStatus = () => {
-  throw new Promise(resolves => null);
+  console.log("laod status")
+  throw new Promise(resolves => setTimeout(resolves,3000));
 }
 
 function Status() {
   const status = loadStatus();
   return <h1>status: {status}</h1>
 }
+
+// safe(loadStatus);
+
+// function safe(fn) {
+//   try{
+//     fn();
+//   } catch(error) {
+//     if(error instanceof Promise) {
+//       error.then(()=>safe(fn));
+//     } else {
+//       throw error;
+//     }
+    
+//   }
+// }
 
 function App() {
   return (

9ecbca856f16731b6f5ec6c4f8b0c44d22df1504
Shin Endo <end3mame@gmail.com>
2023-08-06 09:43:29 +0900

    第10章　テスト - ESlint

---
+++ chapter10/lesson1/sample.js
@@ -0,0 +1,25 @@
+// const gnar = "gnarly";
+
+// const info = ({
+//     file = __filename,
+//     dir = __dirname
+// }) => (
+//     <p>{dir}: {file}</p>
+// );
+
+// switch(gnar) {
+//     default:
+//         console.log("gnarly");
+//         break;
+// }
+
+// function gnar() {
+//     const [nickname, setNickname] = useState(
+//         "dube"
+//     );
+//     return <h1>gnarly</h1>
+// }
+
+function Image() {
+    return <img src="/img.png"/>
+}

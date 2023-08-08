ef9f0de21c3e222ba9c2919d9e72702357417bb5
Shin Endo <end3mame@gmail.com>
2023-08-09 00:02:37 +0900

    第10章　テスト　 - 要素の検索

--- chapter10/lesson6/src/Star.test.js
+++ chapter10/lesson6/src/Star.test.js
@@ -6,5 +6,11 @@ import { toHaveAttribute } from "@testing-library/jest-dom";
 
 test("render a star", async () => {
     render(<Star />);
-    expect(document.querySelector("svg")).toBeTruthy();
+    expect(document.querySelector("svg")).toHaveAttribute("id","star");
 });
+
+test("renders an h1", () => {
+    const { getByText } = render(<Star />);
+    const h1 = getByText(/Great Star/);
+    expect(h1).toHaveTextContent("Great Star");
+})
